61a45f92797316d5230cbb116f7adf89337a93e0
邹景立 <zoujingli@qq.com>
2022-11-23 17:58:17 +0800

    修改组件

--- src/Register.php
+++ src/Register.php
@@ -4,17 +4,24 @@ namespace app\admin;
 
 use think\Service;
 
+/**
+ * 组件注册服务
+ * Class Register
+ * @package app\admin
+ */
 class Register extends Service
 {
+    /**
+     * 指定版本号
+     * @var string
+     */
     const VERSION = '0.0.1';
 
     public function boot(): void
     {
-
     }
 
     public function register(): void
     {
-
     }
 }
