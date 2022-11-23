5ffb22c4092fcffb9c27de3ff6578d46d04e8b8d
邹景立 <zoujingli@qq.com>
2022-11-23 17:56:27 +0800

    初始化文件

---
+++ src/Register.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\admin;
+
+use think\Service;
+
+class Register extends Service
+{
+    const VERSION = '0.0.1';
+
+    public function boot(): void
+    {
+
+    }
+
+    public function register(): void
+    {
+
+    }
+}
