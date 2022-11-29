1955e5d3d365d2210a2b238a637ab39e8ba6de82
邹景立 <zoujingli@qq.com>
2022-11-29 15:15:43 +0800

    同步插件安装机制

--- src/Service.php
+++ src/Service.php
@@ -2,28 +2,23 @@
 
 namespace app\admin;
 
+use think\admin\service\PluginService;
+
 /**
  * 组件注册服务
  * Class Service
  * @package app\admin
  */
-class Service extends \think\Service
+class Service extends PluginService
 {
     /**
-     * 指定版本号
-     * @var string
-     */
-    const VERSION = '0.0.1';
-
-    /**
-     * 配置组件服务
+     * 服务启动注册
      * @return void
      */
     public function boot(): void
     {
-        $attr = explode('\\', __NAMESPACE__);
-        $addons = $this->app->config->get('app.addons', []);
-        $addons[array_pop($attr)] = __DIR__ . DIRECTORY_SEPARATOR . '@' . join('\\', $attr);
-        $this->app->config->set(['addons' => $addons], 'app');
+        // 定义安装复制目录
+        $this->copyPath = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'stc';
+        parent::boot();
     }
 }
