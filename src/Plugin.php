da8d7377457f3b417cc05002b6503bac0bf481ec
邹景立 <zoujingli@qq.com>
2022-12-02 18:08:07 +0800

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -69,9 +69,8 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 
         // 注册指令并执行安装指令
         $dispatcher = $composer->getEventDispatcher();
-        foreach (['service:discover', 'xadmin:publish'] as $command) {
-            $dispatcher->addListener('post-think-admin', "@php think {$command}");
-        }
+        $dispatcher->addListener('post-think-admin', '@php think service:discover');
+        $dispatcher->addListener('post-think-admin', '@php think xadmin:publish');
         $dispatcher->dispatch('post-think-admin');
     }
 }
