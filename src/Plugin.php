4c81b1843220c904202e51cb995c4dea2c2e1714
邹景立 <zoujingli@qq.com>
2022-12-02 16:46:49 +0800

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,5 +1,18 @@
 <?php
 
+// +----------------------------------------------------------------------
+// | ThinkAdmin
+// +----------------------------------------------------------------------
+// | 版权所有 2014~2022 广州楚才信息科技有限公司 [ http://www.cuci.cc ]
+// +----------------------------------------------------------------------
+// | 官方网站: https://thinkadmin.top
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// | 免费声明 ( https://thinkadmin.top/disclaimer )
+// +----------------------------------------------------------------------
+// | gitee 代码仓库：https://gitee.com/zoujingli/think-plugs-admin
+// +----------------------------------------------------------------------
+
 namespace app\admin;
 
 use Composer\Composer;
@@ -8,6 +21,11 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
 
+/**
+ * Composer Plugin
+ * Class Plugin
+ * @package app\admin
+ */
 class Plugin implements PluginInterface, EventSubscriberInterface
 {
     public function activate(Composer $composer, IOInterface $io)
@@ -33,16 +51,23 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 
     public function postAutoloadDump(Event $event)
     {
-        $root = dirname($event->getComposer()->getConfig()->get('vendor-dir'));
-        // 初始化指令入口文件
+
+        // 获取当前环境变量
+        $composer = $event->getComposer();
+        $root = dirname($composer->getConfig()->get('vendor-dir'));
+
+        // 初始化指令入口文件，方便后面执行安装指令
         if (!file_exists($file = "{$root}/think")) {
             copy(dirname(__DIR__) . '/stc/sysroot/think', $file);
         }
-        // 初始化系统缓存配置文件
+
+        // 初始化系统缓存配置文件，没有时候会报错，无法执行安装
         if (!file_exists($file = "{$root}/config/cache.php")) {
             file_exists(dirname($file)) or mkdir(dirname($file));
             copy(dirname(__DIR__) . '/stc/config/cache.php', $file);
         }
-        $event->getComposer()->getEventDispatcher()->dispatchScript('@php think xadmin:publish');
+
+        // 执行应用插件安装指令
+        $composer->getEventDispatcher()->dispatchScript('@php think xadmin:publish');
     }
 }
