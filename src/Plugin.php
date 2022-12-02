b66568344cd72b98f69b11f26310f95f58b9f311
邹景立 <zoujingli@qq.com>
2022-12-02 15:49:26 +0800

    修改插件模式

---
+++ src/Plugin.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\admin;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'post-autoload-dump' => [
+                ['postAutoloadDump', 0],
+            ],
+        ];
+    }
+
+    public function postAutoloadDump(Event $event)
+    {
+        $root = dirname($event->getComposer()->getConfig()->get('vendor-dir'));
+        // 初始化指令入口文件
+        if (!file_exists($file = "{$root}/think")) {
+            copy(__DIR__ . '/stubs/think.stub', $file);
+        }
+        // 初始化系统缓存配置文件
+        if (!file_exists($file = "{$root}/config/cache.php")) {
+            file_exists(dirname($file)) or mkdir(dirname($file));
+            copy(__DIR__ . '/stubs/cache.stub', $file);
+        }
+        $event->getComposer()->getEventDispatcher()->dispatchScript('@php think vendor:publish');
+    }
+}
