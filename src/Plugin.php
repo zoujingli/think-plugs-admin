4778638abf0a28b30cdecb86ea10461452e07b90
邹景立 <zoujingli@qq.com>
2022-12-05 14:59:18 +0800

    优化插件化机制

--- src/Plugin.php
+++ src/Plugin.php
@@ -29,18 +29,6 @@ use think\admin\extend\ToolsExtend;
 class Plugin implements PluginInterface, EventSubscriberInterface
 {
     /**
-     * 应用根目录
-     * @var string
-     */
-    protected $root;
-
-    /**
-     * 当前应用类型
-     * @var string
-     */
-    protected $type;
-
-    /**
      * 当前操作对象
      * @var Composer
      */
@@ -48,17 +36,7 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 
     public function activate(Composer $composer, IOInterface $io)
     {
-        // 变量赋值
         $this->composer = $composer;
-        // 项目根目录
-        $this->root = dirname($composer->getConfig()->get('vendor-dir'));
-        // 默认项目
-        $json = json_decode(file_get_contents("{$this->root}/composer.json"), true);
-        if (empty($json['type']) && empty($json['name'])) {
-            $this->type = 'project';
-        } else {
-            $this->type = $composer->getPackage()->getType();
-        }
     }
 
     public function deactivate(Composer $composer, IOInterface $io)
@@ -87,24 +65,32 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 
     public function preAutoloadDump()
     {
-        if ($this->type === 'project') {
+        $root = dirname($this->composer->getConfig()->get('vendor-dir'));
+        $json = json_decode(file_get_contents("{$root}/composer.json"), true);
+        if (empty($json['type']) && empty($json['name'])) {
+            $type = 'project';
+        } else {
+            $type = $this->composer->getPackage()->getType();
+        }
 
-            // 注册自动加载规格
+        if ($type === 'project') {
+
+            // 注册自动加载规则
             $auto = $this->composer->getPackage()->getAutoload();
             if (empty($auto)) $this->composer->getPackage()->setAutoload([
                 'psr-0' => ['' => 'extend'], 'psr-4' => ['app\\' => 'app'],
             ]);
 
-            // 初始化指令入口文件，方便后面执行安装指令
-            if (!file_exists($file = "{$this->root}/think")) {
+            // 初始化指令入口 ( 后面需要执行安装指令 )
+            if (!file_exists($file = "{$root}/think")) {
                 copy(dirname(__DIR__) . '/stc/sysroot/think', $file);
             }
 
-            // 初始化系统配置文件 ( 没有的时候会报错无法执行安装 )
-            ToolsExtend::copyfile(dirname(__DIR__) . '/stc/config', "{$this->root}/config", [], false, false);
+            // 初始化配置文件 ( 没有的时候会报错无法执行安装 )
+            ToolsExtend::copyfile(dirname(__DIR__) . '/stc/config', "{$root}/config", [], false, false);
 
-            // 初始化应用入口应用
-            if (!file_exists($file = "{$this->root}/app/index/controller/Index.php")) {
+            // 初始化应用入口
+            if (!file_exists($file = "{$root}/app/index/controller/Index.php")) {
                 (file_exists(dirname($file)) or mkdir(dirname($file), 0755, true)) && file_put_contents($file, '<?php'
                     . "\n\nnamespace app\index\controller;"
                     . "\n\nclass Index extends \\think\\admin\\Controller {"
@@ -112,10 +98,15 @@ class Plugin implements PluginInterface, EventSubscriberInterface
                     . "\n}\n");
             }
 
-            // 注册初始化指令并执行相关安装指令
+            // 自动注册执行指令
             $dispatcher = $this->composer->getEventDispatcher();
-            $dispatcher->addListener('post-autoload-dump', '@php think service:discover');
-            $dispatcher->addListener('post-autoload-dump', '@php think xadmin:publish');
+            $postScripts = $json['scripts']['post-autoload-dump'] ?? [];
+            if (!in_array($script = '@php think service:discover', $postScripts)) {
+                $dispatcher->addListener('post-autoload-dump', $script);
+            }
+            if (!in_array($script = '@php think xadmin:publish', $postScripts)) {
+                $dispatcher->addListener('post-autoload-dump', $script);
+            }
         }
     }
 
