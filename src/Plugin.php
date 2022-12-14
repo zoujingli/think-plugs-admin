4923cf56e10cade6cf161b2f42a5e02764a6180d
邹景立 <zoujingli@qq.com>
2022-12-14 15:43:26 +0800

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -36,17 +36,18 @@ class Plugin implements PluginInterface
      */
     public function activate(Composer $composer, IOInterface $io)
     {
-        $rootPath = dirname($composer->getConfig()->get('vendor-dir'));
-        $rootJson = (new JsonFile("{$rootPath}/composer.json"))->read();
+        $rootJson = (new JsonFile('composer.json'))->read();
+
         // 检测配置状态
         $pluginUrl = CodeExtend::deSafe64('aHR0cHM6Ly9vcGVuLmN1Y2kuY2MvcGx1Z2lu');
         foreach ($rootJson['repositories'] ?? [] as $item) if (empty($pluginCenter) && isset($item['url'])) {
             if (is_numeric(strpos($item['url'], $pluginUrl))) $pluginCenter = true;
         }
+
         // 临时动态注册
         if (empty($pluginCenter)) {
             $manager = $composer->getRepositoryManager();
-            $manager->prependRepository($manager->createRepository('composer', ['url' => $pluginUrl, "canonical" => false]));
+            $manager->prependRepository($manager->createRepository('composer', ['url' => $pluginUrl, 'canonical' => false]));
         }
 
         // 如果项目类型配置
@@ -55,7 +56,7 @@ class Plugin implements PluginInterface
             // 动态修改项目配置
             if (empty($pluginCenter)) {
                 $composer->getConfig()->getConfigSource()->addRepository('plugins', [
-                    "url" => $pluginUrl, "type" => "composer", "canonical" => false
+                    'url' => $pluginUrl, 'type' => 'composer', 'canonical' => false
                 ]);
             }
 
@@ -67,22 +68,18 @@ class Plugin implements PluginInterface
 
 
             // 初始化指令入口 ( 后面需要执行安装指令 )
-            if (!file_exists($file = "{$rootPath}/think")) {
+            if (!file_exists($file = 'think')) {
                 copy(dirname(__DIR__) . '/stc/sysroot/think', $file);
             }
 
             // 初始化配置文件 ( 没有的时候会报错无法执行安装 )
-            ToolsExtend::copyfile(dirname(__DIR__) . '/stc/config', "{$rootPath}/config", [], false, false);
+            ToolsExtend::copyfile(dirname(__DIR__) . '/stc/config', 'config', [], false, false);
 
             // 初始化应用入口 （ 主要是用来跳转到后台管理入口 ）
-            if (!file_exists($file = "{$rootPath}/app/index/controller/Index.php")) {
-                if (file_exists(dirname($file)) or mkdir(dirname($file), 0755, true)) {
-                    file_put_contents($file, '<?php'
-                        . "\n\nnamespace app\index\controller;"
-                        . "\n\nclass Index extends \\think\\admin\\Controller\n{"
-                        . "\n\tpublic function index() {\n\t\t\$this->redirect(sysuri('admin/login/index'));\n\t}"
-                        . "\n}\n");
-                }
+            if (!file_exists($file = 'app/index/controller/Index.php')) {
+                if (file_exists(dirname($file)) || mkdir(dirname($file), 0755, true)) file_put_contents($file,
+                    '<?php' . "\n\nnamespace app\index\controller;\n\nclass Index extends \\think\\admin\\Controller\n{"
+                    . "\n\tpublic function index()\n\t{\n\t\t\$this->redirect(sysuri('admin/login/index'));\n\t}\n}\n");
             }
 
             // 自动注册执行指令
