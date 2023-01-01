d9cb287edbbc5077452ffa2e9eab6e4603c23c1b
邹景立 <zoujingli@qq.com>
2023-01-01 14:21:28 +0800

    修改版本注释，去除服务端更新接口

--- stc/database/20221013031926_install_admin_data.php
+++ stc/database/20221013031926_install_admin_data.php
@@ -1,5 +1,18 @@
 <?php
 
+// +----------------------------------------------------------------------
+// | Admin Plugin for ThinkAdmin
+// +----------------------------------------------------------------------
+// | 版权所有 2014~2023 Anyon<zoujingli@qq.com>
+// +----------------------------------------------------------------------
+// | 官方网站: https://thinkadmin.top
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// | 免费声明 ( https://thinkadmin.top/disclaimer )
+// +----------------------------------------------------------------------
+// | gitee 代码仓库：https://gitee.com/zoujingli/think-plugs-admin
+// +----------------------------------------------------------------------
+
 use think\admin\extend\PhinxExtend;
 use think\admin\model\SystemConfig;
 use think\admin\model\SystemUser;
