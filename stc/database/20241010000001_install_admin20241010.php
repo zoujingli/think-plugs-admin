ce2b93d800554cdb1a879091b1678c4275a53b0c
邹景立 <zoujingli@qq.com>
2024-10-17 15:24:54 +0800

    fix: 重新调整脚本顺序

--- stc/database/20241010000001_install_admin20241010.php
+++ stc/database/20241010000001_install_admin20241010.php
@@ -14,10 +14,7 @@
 // | github 代码仓库：https://github.com/zoujingli/think-plugs-admin
 // +----------------------------------------------------------------------
 
-use app\admin\Service;
 use think\admin\extend\PhinxExtend;
-use think\admin\model\SystemConfig;
-use think\admin\model\SystemUser;
 use think\migration\Migrator;
 
 @set_time_limit(0);
@@ -26,7 +23,7 @@ use think\migration\Migrator;
 /**
  * 系统模块数据
  */
-class InstallAdmin20241016 extends Migrator
+class InstallAdmin20241010 extends Migrator
 {
 
     /**
@@ -53,9 +50,6 @@ class InstallAdmin20241016 extends Migrator
         $this->_create_system_oplog();
         $this->_create_system_queue();
         $this->_create_system_user();
-        $this->insertUser();
-        $this->insertMenu();
-        $this->insertConf();
     }
 
     /**
@@ -335,61 +329,5 @@ class InstallAdmin20241016 extends Migrator
         ], [
             'sort', 'status', 'username', 'is_deleted',
         ], true);
-    }
-
-    /**
-     * 初始化用户数据
-     * @return void
-     */
-    private function insertUser()
-    {
-        $model = SystemUser::mk()->whereRaw('1=1')->findOrEmpty();
-        $model->isEmpty() && $model->save([
-            'id'       => '10000',
-            'username' => 'admin',
-            'nickname' => '超级管理员',
-            'password' => '21232f297a57a5a743894a0e4a801fc3',
-            'headimg'  => 'https://thinkadmin.top/static/img/head.png',
-        ]);
-    }
-
-    /**
-     * 初始化配置参数
-     * @return void
-     */
-    private function insertConf()
-    {
-        $modal = SystemConfig::mk()->whereRaw('1=1')->findOrEmpty();
-        $modal->isEmpty() && $modal->insertAll([
-            ['type' => 'base', 'name' => 'app_name', 'value' => 'ThinkAdmin'],
-            ['type' => 'base', 'name' => 'app_version', 'value' => 'v6'],
-            ['type' => 'base', 'name' => 'editor', 'value' => 'ckeditor5'],
-            ['type' => 'base', 'name' => 'login_name', 'value' => '系统管理'],
-            ['type' => 'base', 'name' => 'site_copy', 'value' => '©版权所有 2014-' . date('Y') . ' ThinkAdmin'],
-            ['type' => 'base', 'name' => 'site_icon', 'value' => 'https://thinkadmin.top/static/img/logo.png'],
-            ['type' => 'base', 'name' => 'site_name', 'value' => 'ThinkAdmin'],
-            ['type' => 'base', 'name' => 'site_theme', 'value' => 'default'],
-            ['type' => 'storage', 'name' => 'allow_exts', 'value' => 'doc,gif,ico,jpg,mp3,mp4,p12,pem,png,zip,rar,xls,xlsx'],
-            ['type' => 'storage', 'name' => 'type', 'value' => 'local'],
-            ['type' => 'wechat', 'name' => 'type', 'value' => 'api'],
-        ]);
-    }
-
-    /**
-     * 初始化系统菜单
-     * @return void
-     */
-    private function insertMenu()
-    {
-        // 初始化菜单数据
-        PhinxExtend::write2menu([
-            [
-                'name' => '系统管理',
-                'sort' => '100',
-                'subs' => Service::menu(),
-            ],
-        ], [
-            'url|node' => 'admin/config/index'
-        ]);
     }
 }
