d2e881f78deb2423414573c873a8d74269727d15
Anyon <zoujingli@qq.com>
2026-04-02 13:20:53 +0800

    style(runtime): 统一入口脚本与迁移文件格式

--- stc/database/20241010000002_install_admin20241011.php
+++ stc/database/20241010000002_install_admin20241011.php
@@ -17,7 +17,6 @@ declare(strict_types=1);
  * | github 代码仓库：https://github.com/zoujingli/ThinkAdmin
  * +----------------------------------------------------------------------
  */
-
 use app\admin\Service;
 use think\admin\extend\PhinxExtend;
 use think\admin\model\SystemConfig;
